ca319eabb3723bd07d88ddf7f1d151ad7b54ef83
follatto95 <malenottinicco@gmail.com>
2025-06-01 18:13:18 +0200

    modified:   Dockerfile 	new file:   pkg/mqttclient/mqtt.go 	new file:   proto/data.proto

---
+++ pkg/mqttclient/mqtt.go
@@ -0,0 +1,53 @@
+package mqttclient
+
+import (
+	"log"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"google.golang.org/protobuf/proto"
+
+	"yourproject/proto" // importa correttamente il package
+)
+
+type MQTTClient struct {
+	client mqtt.Client
+}
+
+func NewMQTTClient(broker, clientID string) *MQTTClient {
+	opts := mqtt.NewClientOptions().
+		AddBroker(broker).
+		SetClientID(clientID).
+		SetKeepAlive(2 * time.Second).
+		SetPingTimeout(1 * time.Second)
+
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatalf("MQTT connect error: %v", token.Error())
+	}
+
+	return &MQTTClient{client: client}
+}
+
+func (m *MQTTClient) Publish(topic string, data *meshspy.NodeData) error {
+	payload, err := proto.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	token := m.client.Publish(topic, 0, false, payload)
+	token.Wait()
+	return token.Error()
+}
+
+func (m *MQTTClient) Subscribe(topic string, handler func(*meshspy.NodeData)) {
+	m.client.Subscribe(topic, 0, func(client mqtt.Client, msg mqtt.Message) {
+		var data meshspy.NodeData
+		err := proto.Unmarshal(msg.Payload(), &data)
+		if err != nil {
+			log.Printf("Invalid protobuf: %v", err)
+			return
+		}
+		handler(&data)
+	})
+}
